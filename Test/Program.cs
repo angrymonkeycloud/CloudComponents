41ecc1a9f3fce33b439f17905e9ee11076fcdcf6
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2021-07-14 15:53:30 +0300

    Add styles to Progress Bar

--- Test/Program.cs
+++ Test/Program.cs
@@ -19,7 +19,7 @@ namespace AngryMonkey.Cloud.VideoPlayer.Website
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+			
 			await builder.Build().RunAsync();
 		}
 	}
