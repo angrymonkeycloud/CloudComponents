1f984c71f508db474461289769afad112541965f
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2021-07-23 15:00:30 +0300

    Demo added

---
+++ Demo/Program.cs
@@ -0,0 +1,31 @@
+using AngryMonkey.Cloud.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryMonkey.Cloud.Components.Demo
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Services.AddSingleton(sn => new PageBaseSettings()
+			{
+				DefaultTitle = "Angry Monkey Cloud Components Demo"
+			});
+			
+			await builder.Build().RunAsync();
+		}
+	}
+}
