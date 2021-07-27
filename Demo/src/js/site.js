4307e27241077518560a80e2b022429576ce7e50
Hussein Mroueh <hussein_mroueh93@hotmail.com>
2021-07-27 09:57:11 +0300

    nav bar

---
+++ Demo/src/js/site.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    debugger;
+    var links = document.querySelector(".components-links");
+    if (links.classList.contains("_clicked")) {
+        links.classList.remove("_clicked");
+    }
+    else {
+        links.classList.add("_clicked");
+    }
+}
+//# sourceMappingURL=site.js.map
---
+++ Demo/src/js/site.ts
@@ -0,0 +1,12 @@
+﻿function myFunction(): void {
+   
+    var links = document.querySelector(".components-links");
+   
+    if (links.classList.contains("_clicked")) {
+        links.classList.remove("_clicked");
+    }
+    else
+    {
+        links.classList.add("_clicked");
+    }
+}
