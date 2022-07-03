1358ae408958ef2232e6a848078ac5d2b1920007
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2022-07-03 11:52:12 +0300

    Blazor Server Demo added

---
+++ ServerDemo/src/js/site.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var links = document.querySelector(".components-links");
+    if (links.classList.contains("_clicked")) {
+        links.classList.remove("_clicked");
+    }
+    else {
+        links.classList.add("_clicked");
+    }
+}
+//# sourceMappingURL=site.js.map
---
+++ ServerDemo/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var links = document.querySelector(".components-links");
+    if (links.classList.contains("_clicked")) {
+        links.classList.remove("_clicked");
+    }
+    else {
+        links.classList.add("_clicked");
+    }
+}
