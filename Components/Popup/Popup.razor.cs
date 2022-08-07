f2a81067df443e9b2c3b762cf864d10b7683e8db
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2022-08-07 20:11:55 +0300

    Popup Added

---
+++ Components/Popup/Popup.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryMonkey.Cloud.Components
+{
+	public partial class Popup : IClosable
+	{
+		[Parameter] public RenderFragment ChildContent { get; set; }
+		private ElementReference ComponentElement { get; set; }
+		public bool Visible { get; set; } = false;
+
+		public void Open()
+		{
+			Visible = true;
+		}
+
+		public void Close()
+		{
+			Visible = false;
+		}
+	}
+}
