a9bfc136dde384db58afd0aab579cbccda118be5
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2023-02-20 14:10:35 +0200

    Tabs added

---
+++ Components/Tabs/Models/TabItem.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AngryMonkey.Cloud.Components
+{
+    public class TabItem
+    {
+        public TabItem(string content)
+        {
+            Content= content;
+        }
+
+        public TabItem(Type content)
+        {
+            ContentType = content;
+        }
+
+        public TabItem(Type content, IDictionary<string, object> parameters)
+        {
+            ContentType = content;
+            Content = parameters;
+        }
+
+        public Type ContentType { get; set; }
+        public object Content { get; set; }
+        public bool IsActive { get; set; } = false;
+        public string CssClass
+        {
+            get
+            {
+                List<string> classes = new() { "amc-tab" };
+
+                if (IsActive)
+                    classes.Add("_active");
+
+                return string.Join(" ", classes);
+            }
+        }
+
+        public async Task InvokeOnActivated()
+        {
+            IsActive = true;
+            await OnActivated.InvokeAsync();
+        }
+
+        public EventCallback OnActivated;
+    }
+}
---
+++ Components/Tabs/Tabs.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AngryMonkey.Cloud.Components
+{
+    public partial class Tabs
+    {
+        [Parameter] public List<TabItem> TabsList { get; set; }
+
+        protected override async void OnInitialized()
+        {
+            if (TabsList.Any() && !TabsList.Any(key => key.IsActive))
+                await TabsList.First().InvokeOnActivated();
+
+            base.OnInitialized();
+        }
+
+        protected async Task OnTabClicked(TabItem item)
+        {
+            TabItem activeItem = TabsList.FirstOrDefault(key => key.IsActive);
+
+            if (activeItem != null)
+                activeItem.IsActive = false;
+
+            await item.InvokeOnActivated();
+        }
+    }
+}
