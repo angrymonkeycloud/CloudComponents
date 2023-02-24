be3e6339c0b13090ace520971692c8a2d98bfc25
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2023-02-25 01:30:27 +0200

    fine tuning

--- Components.WebAssmbly/Program.cs
+++ Components.WebAssmbly/Program.cs
@@ -1,6 +1,4 @@
 using AngryMonkey.Cloud.Components;
-using Components.WebAssmbly;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
