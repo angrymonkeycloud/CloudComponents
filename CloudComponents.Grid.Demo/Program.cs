82dbfc5e7eea0b850cb61bef2b3b4624830a45b5
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2026-06-22 12:16:10 +0300

    Cloud Grid Demo added

---
+++ CloudComponents.Grid.Demo/Program.cs
@@ -0,0 +1,12 @@
+using CloudComponents.Grid.Demo;
+using CloudComponents.Grid.Demo.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<SampleDataService>();
+
+await builder.Build().RunAsync();
